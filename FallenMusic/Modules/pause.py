239c42bee5ec6b32153085860a56fbea458305f6
Shejdjdnjdjd <123052381+Shejdjdnjdjd@users.noreply.github.com>
2023-08-28 05:42:58 +0300

    Update pause.py

--- FallenMusic/Modules/pause.py
+++ FallenMusic/Modules/pause.py
@@ -27,7 +27,7 @@ from FallenMusic import app, pytgcalls
 from FallenMusic.Helpers import admin_check, close_key, is_streaming, stream_off
 
 
-@app.on_message(filters.command(["pause"]) | filters.command(["كتم","اسكت","ايقاف مؤقت"],prefixes= ["/", "!","","#"]) & filters.group)
+@app.on_message(filters.command(["pause"]) | filters.command(["ايقاف","اسكتي","اليس اص","سكب"],prefixes= ["/", "!","","#"]) & filters.group)
 @admin_check
 async def pause_str(_, message: Message):
     try:
@@ -37,12 +37,12 @@ async def pause_str(_, message: Message):
 
     if not await is_streaming(message.chat.id):
         return await message.reply_text(
-            "⎊ اكتب كمل او /resume عشان يكمل الاغنيه"
+            "• اكتب كمل او /resume عشان يكمل الاغنيه"
         )
 
     await pytgcalls.pause_stream(message.chat.id)
     await stream_off(message.chat.id)
     return await message.reply_text(
-        text=f"⎊ تم ايقاف التشغيل مؤقتاً 🎧\n \n⎊ بواسطة : {message.from_user.mention} ♥",
+        text=f"• تم ايقاف التشغيل مؤقتاً .\n \n• بواسطة : {message.from_user.mention} .",
         reply_markup=close_key,
     )
